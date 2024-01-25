8d647da7935a6f8ac7208fd4e77e552feb3b4190
Romain Francois <romain@purrple.cat>
2024-01-25 10:59:26 +0100

    initial pkg setup

---
+++ r/R/duckdb_con.R
@@ -0,0 +1,9 @@
+#' Open a connection to duckdb with the rfuns extension loaded
+#'
+#' @return a duckdb connection
+con <- function() {
+  con <- DBI::dbConnect(duckdb::duckdb(config=list('allow_unsigned_extensions' = 'true')))
+  extension <- system.file("extension", "rfuns.duckdb_extension", package = "duckdbrfuns")
+  DBI::dbExecute(con, paste0("LOAD '", extension, "'"))
+  con
+}
