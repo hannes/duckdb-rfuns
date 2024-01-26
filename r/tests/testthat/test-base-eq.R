99ceb951346fe860b9f9143256b3d56310c86872
Kirill Müller <kirill@cynkra.com>
2024-01-26 04:51:45 +0100

    test: Example for relational API

---
+++ r/tests/testthat/test-base-eq.R
@@ -0,0 +1,21 @@
+# To be generated from test.R
+
+test_that("int + int", {
+  con <- con(); on.exit(dbDisconnect(con, shutdown=TRUE))
+
+  in_df <- data.frame(a = 0L, b = 0L)
+  in_rel <- duckdb:::rel_from_df(con, in_df)
+  out_rel <-
+    duckdb:::rel_project(
+      in_rel,
+      list(duckdb:::expr_function(
+        "r_base::==",
+        list(
+          duckdb:::expr_reference("a"),
+          duckdb:::expr_reference("b")
+        )
+      ))
+    )
+  out_df <- duckdb:::rel_to_altrep(out_rel)
+  expect_identical(out_df[[1]], TRUE)
+})
