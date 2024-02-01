bc804460deb8df3867ce86057d5f2343ab863ddf
Romain Francois <romain@purrple.cat>
2024-02-01 17:27:43 +0100

    there seems to be an issue with having a file named con.Rd

--- duckdb-rfuns-r/R/duckdb_con.R
+++ duckdb-rfuns-r/R/duckdb_con.R
@@ -1,6 +1,7 @@
 #' Open a connection to duckdb with the rfuns extension loaded
 #'
 #' @return a duckdb connection
+#' @rdname duckdb_con
 con <- function() {
   con <- DBI::dbConnect(duckdb::duckdb(config=list('allow_unsigned_extensions' = 'true')))
   extension <- system.file("extension", "rfuns.duckdb_extension", package = "duckdbrfuns")
