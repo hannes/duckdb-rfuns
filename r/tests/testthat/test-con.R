86cda8925c762c71b4df87dcc4d91ee166d32562
Romain Francois <romain@purrple.cat>
2024-02-01 15:14:32 +0100

    local_con() test fixture from #14

--- r/tests/testthat/test-con.R
+++ r/tests/testthat/test-con.R
@@ -1,5 +1,5 @@
 test_that("con() load the extension", {
-  con <- con(); on.exit(dbDisconnect(con, shutdown=TRUE))
+  con <- local_con()
 
   expect_equal(dbGetQuery(con, 'SELECT "r_base::+"(1, 2)')[,1], 3)
 })
