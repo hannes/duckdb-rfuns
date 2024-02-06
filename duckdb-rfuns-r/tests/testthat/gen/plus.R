5178eb734d1380756ff150a2c4dc5c0e4f4b8a49
Romain Francois <romain@purrple.cat>
2024-02-06 10:34:35 +0100

    tests

--- duckdb-rfuns-r/tests/testthat/gen/plus.R
+++ duckdb-rfuns-r/tests/testthat/gen/plus.R
@@ -9,3 +9,12 @@ NA_real_ + 2
 1L + 2L
 NA_integer_ + 2L
 1L + NA_integer_
+
+"<int> + <dbl>"
+1L + 3.0
+3.0 + 1L
+1L + NA_real_
+NA_real_ + 1L
+NA_integer_ + 4.0
+4.0 + NA_integer_
+NA_integer_ + NA_real_
