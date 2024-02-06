bbf8acd55bfec20bcf9361d8fdaedd1f581c9aa1
Romain Francois <romain@purrple.cat>
2024-02-06 11:49:46 +0100

    tweaks to please R CMD check

---
+++ duckdb-rfuns-r/R/zzz.R
@@ -0,0 +1,7 @@
+# silence R CMD check
+# TODO: some of these are testthat only dependencies
+#' @importFrom tibble tibble
+#' @importFrom constructive construct
+#' @importFrom withr defer_parent
+#' @importFrom DBI dbConnect dbDisconnect dbGetQuery
+NULL
