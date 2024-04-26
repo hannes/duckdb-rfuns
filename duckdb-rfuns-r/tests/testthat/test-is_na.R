37356c6b9a20cae3cb7df16e88241a73b1013651
Romain Francois <romain@tada.science>
2024-04-26 13:41:45 +0000

    isna_any, and isna_double instead of template isna

--- duckdb-rfuns-r/tests/testthat/test-is_na.R
+++ duckdb-rfuns-r/tests/testthat/test-is_na.R
@@ -1,24 +1,28 @@
 test_that("rfuns_isna(<double>)", {
-  expect_equal(rfuns_is.na(c(1, 2, NA, NaN)), c(FALSE, FALSE, TRUE, TRUE))
-  expect_equal(rfuns_is.na(1)               , FALSE)
-  expect_equal(rfuns_is.na(NA_real_)        , TRUE)
-  expect_equal(rfuns_is.na(NaN)             , TRUE)
+  x <- c(1, 2, NA, NaN)
+  expect_equal(rfuns_is.na(x)         , is.na(x))
+  expect_equal(rfuns_is.na(1)         , FALSE)
+  expect_equal(rfuns_is.na(NA_real_)  , TRUE)
+  expect_equal(rfuns_is.na(NaN)       , TRUE)
 })
 
 test_that("rfuns_isna(<int>)", {
-  expect_equal(rfuns_is.na(c(1L, NA_integer_)), c(FALSE, TRUE))
-  expect_equal(rfuns_is.na(1L)                , FALSE)
-  expect_equal(rfuns_is.na(NA_integer_)       , TRUE)
+  x <- c(1L, NA_integer_)
+  expect_equal(rfuns_is.na(x)           , is.na(x))
+  expect_equal(rfuns_is.na(1L)          , FALSE)
+  expect_equal(rfuns_is.na(NA_integer_) , TRUE)
 })
 
 test_that("rfuns_isna(<logical>)", {
-  expect_equal(rfuns_is.na(c(TRUE, NA)) , c(FALSE, TRUE))
-  expect_equal(rfuns_is.na(TRUE)        , FALSE)
-  expect_equal(rfuns_is.na(NA)          , TRUE)
+  x <- c(TRUE, NA)
+  expect_equal(rfuns_is.na(x)    , is.na(x))
+  expect_equal(rfuns_is.na(TRUE) , FALSE)
+  expect_equal(rfuns_is.na(NA)   , TRUE)
 })
 
 test_that("rfuns_isna(<string>)", {
-  expect_equal(rfuns_is.na(c("", NA_character_)), c(FALSE, TRUE))
-  expect_equal(rfuns_is.na("NA")                , FALSE)
-  expect_equal(rfuns_is.na(NA_character_)       , TRUE)
+  x <- c("", NA_character_)
+  expect_equal(rfuns_is.na(x)             , is.na(x))
+  expect_equal(rfuns_is.na("NA")          , FALSE)
+  expect_equal(rfuns_is.na(NA_character_) , TRUE)
 })
--- src/is_na.cpp
+++ src/is_na.cpp
@@ -8,32 +8,11 @@
 namespace duckdb {
 namespace rfuns {
 
-template <bool is_double>
-struct IsNaWrapper {
-	IsNaWrapper(Vector&){}
-
-	inline bool is_na(idx_t) {
-		return false;
-	}
-};
-
-template <>
-struct IsNaWrapper<true> {
-	IsNaWrapper(Vector& input) : data(FlatVector::GetData<double>(input)){}
-
-	inline bool is_na(idx_t idx) {
-		return isnan(data[idx]);
-	}
-
-	double* data;
-};
-
-template <bool is_double>
-void isna(DataChunk &args, ExpressionState &state, Vector &result) {
+void isna_double(DataChunk &args, ExpressionState &state, Vector &result) {
 	auto count = args.size();
 	auto input = args.data[0];
 	auto mask = FlatVector::Validity(input);
-	auto wrapper = IsNaWrapper<is_double>(input);
+	auto* data = FlatVector::GetData<double>(input);
 
 	result.SetVectorType(VectorType::FLAT_VECTOR);
 	auto result_data = FlatVector::GetData<bool>(result);
@@ -47,7 +26,7 @@ void isna(DataChunk &args, ExpressionState &state, Vector &result) {
 		if (ValidityMask::AllValid(validity_entry)) {
 			// all valid: check with isnan()
 			for (; base_idx < next; base_idx++) {
-				result_data[base_idx] = wrapper.is_na(base_idx);
+				result_data[base_idx] = isnan(data[base_idx]);
 			}
 		} else if (ValidityMask::NoneValid(validity_entry)) {
 			// None valid:
@@ -60,7 +39,7 @@ void isna(DataChunk &args, ExpressionState &state, Vector &result) {
 			for (; base_idx < next; base_idx++) {
 				if (ValidityMask::RowIsValid(validity_entry, base_idx - start)) {
 					D_ASSERT(mask.RowIsValid(base_idx));
-					result_data[base_idx] = wrapper.is_na(base_idx);
+					result_data[base_idx] = isnan(data[base_idx]);
 				} else {
 					result_data[base_idx] = true;
 				}
@@ -69,11 +48,46 @@ void isna(DataChunk &args, ExpressionState &state, Vector &result) {
 	}
 }
 
+void isna_any(DataChunk &args, ExpressionState &state, Vector &result) {
+	auto count = args.size();
+	auto input = args.data[0];
+	auto mask = FlatVector::Validity(input);
+
+	result.SetVectorType(VectorType::FLAT_VECTOR);
+	auto result_data = FlatVector::GetData<bool>(result);
+
+	idx_t base_idx = 0;
+	auto entry_count = ValidityMask::EntryCount(count);
+	for (idx_t entry_idx = 0; entry_idx < entry_count; entry_idx++) {
+		auto validity_entry = mask.GetValidityEntry(entry_idx);
+		idx_t next = MinValue<idx_t>(base_idx + ValidityMask::BITS_PER_VALUE, count);
+
+		if (ValidityMask::AllValid(validity_entry)) {
+			// all valid: check with isnan()
+			for (; base_idx < next; base_idx++) {
+				result_data[base_idx] = false;
+			}
+		} else if (ValidityMask::NoneValid(validity_entry)) {
+			// None valid:
+			for (; base_idx < next; base_idx++) {
+				result_data[base_idx] = true;
+			}
+		} else {
+			// partially valid: need to check individual elements for validity
+			idx_t start = base_idx;
+			for (; base_idx < next; base_idx++) {
+				result_data[base_idx] = !ValidityMask::RowIsValid(validity_entry, base_idx - start);
+			}
+		}
+	}
+}
+
+
 ScalarFunctionSet base_r_is_na() {
 	ScalarFunctionSet set("r_base::is.na");
 
-	set.AddFunction(ScalarFunction({LogicalType::DOUBLE}, LogicalType::BOOLEAN, isna<true>));
-	set.AddFunction(ScalarFunction({LogicalType::ANY}, LogicalType::BOOLEAN, isna<false>));
+	set.AddFunction(ScalarFunction({LogicalType::DOUBLE}, LogicalType::BOOLEAN, isna_double));
+	set.AddFunction(ScalarFunction({LogicalType::ANY}   , LogicalType::BOOLEAN, isna_any));
 
 	return set;
 }
