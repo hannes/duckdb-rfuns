24d014a95ab7e891b6344370051c67de0017b984
Romain Francois <romain@tada.science>
2024-05-07 09:13:48 +0000

    handle FLAT and CONSTANT

--- src/is_na.cpp
+++ src/is_na.cpp
@@ -11,39 +11,61 @@ namespace rfuns {
 void isna_double(DataChunk &args, ExpressionState &state, Vector &result) {
 	auto count = args.size();
 	auto input = args.data[0];
-	auto mask = FlatVector::Validity(input);
-	auto* data = FlatVector::GetData<double>(input);
 
-	result.SetVectorType(VectorType::FLAT_VECTOR);
-	auto result_data = FlatVector::GetData<bool>(result);
+	switch(input.GetVectorType()) {
+		case VectorType::FLAT_VECTOR: {
+			auto mask = FlatVector::Validity(input);
+			auto* data = FlatVector::GetData<double>(input);
 
-	idx_t base_idx = 0;
-	auto entry_count = ValidityMask::EntryCount(count);
-	for (idx_t entry_idx = 0; entry_idx < entry_count; entry_idx++) {
-		auto validity_entry = mask.GetValidityEntry(entry_idx);
-		idx_t next = MinValue<idx_t>(base_idx + ValidityMask::BITS_PER_VALUE, count);
+			result.SetVectorType(VectorType::FLAT_VECTOR);
+			auto result_data = FlatVector::GetData<bool>(result);
 
-		if (ValidityMask::AllValid(validity_entry)) {
-			// all valid: check with isnan()
-			for (; base_idx < next; base_idx++) {
-				result_data[base_idx] = isnan(data[base_idx]);
-			}
-		} else if (ValidityMask::NoneValid(validity_entry)) {
-			// None valid:
-			for (; base_idx < next; base_idx++) {
-				result_data[base_idx] = true;
-			}
-		} else {
-			// partially valid: need to check individual elements for validity
-			idx_t start = base_idx;
-			for (; base_idx < next; base_idx++) {
-				if (ValidityMask::RowIsValid(validity_entry, base_idx - start)) {
-					D_ASSERT(mask.RowIsValid(base_idx));
-					result_data[base_idx] = isnan(data[base_idx]);
+			idx_t base_idx = 0;
+			auto entry_count = ValidityMask::EntryCount(count);
+			for (idx_t entry_idx = 0; entry_idx < entry_count; entry_idx++) {
+				auto validity_entry = mask.GetValidityEntry(entry_idx);
+				idx_t next = MinValue<idx_t>(base_idx + ValidityMask::BITS_PER_VALUE, count);
+
+				if (ValidityMask::AllValid(validity_entry)) {
+					// all valid: check with isnan()
+					for (; base_idx < next; base_idx++) {
+						result_data[base_idx] = isnan(data[base_idx]);
+					}
+				} else if (ValidityMask::NoneValid(validity_entry)) {
+					// None valid:
+					for (; base_idx < next; base_idx++) {
+						result_data[base_idx] = true;
+					}
 				} else {
-					result_data[base_idx] = true;
+					// partially valid: need to check individual elements for validity
+					idx_t start = base_idx;
+					for (; base_idx < next; base_idx++) {
+						if (ValidityMask::RowIsValid(validity_entry, base_idx - start)) {
+							D_ASSERT(mask.RowIsValid(base_idx));
+							result_data[base_idx] = isnan(data[base_idx]);
+						} else {
+							result_data[base_idx] = true;
+						}
+					}
 				}
 			}
+			break;
+		}
+
+		case VectorType::CONSTANT_VECTOR: {
+			result.SetVectorType(VectorType::FLAT_VECTOR);
+			auto result_data = ConstantVector::GetData<bool>(result);
+			auto ldata = ConstantVector::GetData<double>(input);
+
+			*result_data = ConstantVector::IsNull(input) || isnan(*ldata);
+
+			break;
+		}
+
+		default: {
+			// TODO: use UnifiedVectorFormat instead
+			throw InvalidInputException("is.na() only handles constant and flat vectors");
+			break;
 		}
 	}
 }
@@ -51,42 +73,63 @@ void isna_double(DataChunk &args, ExpressionState &state, Vector &result) {
 void isna_any(DataChunk &args, ExpressionState &state, Vector &result) {
 	auto count = args.size();
 	auto input = args.data[0];
-	auto mask = FlatVector::Validity(input);
 
-	result.SetVectorType(VectorType::FLAT_VECTOR);
-	auto result_data = FlatVector::GetData<bool>(result);
+	switch(input.GetVectorType()) {
+		case VectorType::FLAT_VECTOR: {
+			auto mask = FlatVector::Validity(input);
 
-	if (mask.AllValid()) {
-		for (idx_t i = 0; i < count; i++) {
-			result_data[i] = false;
+			result.SetVectorType(VectorType::FLAT_VECTOR);
+			auto result_data = FlatVector::GetData<bool>(result);
+
+			if (mask.AllValid()) {
+				for (idx_t i = 0; i < count; i++) {
+					result_data[i] = false;
+				}
+				return;
+			}
+
+			idx_t base_idx = 0;
+			auto entry_count = ValidityMask::EntryCount(count);
+			for (idx_t entry_idx = 0; entry_idx < entry_count; entry_idx++) {
+				auto validity_entry = mask.GetValidityEntry(entry_idx);
+				idx_t next = MinValue<idx_t>(base_idx + ValidityMask::BITS_PER_VALUE, count);
+
+				if (ValidityMask::AllValid(validity_entry)) {
+					// all valid: check with isnan()
+					for (; base_idx < next; base_idx++) {
+						result_data[base_idx] = false;
+					}
+				} else if (ValidityMask::NoneValid(validity_entry)) {
+					// None valid:
+					for (; base_idx < next; base_idx++) {
+						result_data[base_idx] = true;
+					}
+				} else {
+					// partially valid: need to check individual elements for validity
+					idx_t start = base_idx;
+					for (; base_idx < next; base_idx++) {
+						result_data[base_idx] = !ValidityMask::RowIsValid(validity_entry, base_idx - start);
+					}
+				}
+			}
+			break;
 		}
-		return;
+
+		case VectorType::CONSTANT_VECTOR: {
+			result.SetVectorType(VectorType::CONSTANT_VECTOR);
+			auto result_data = ConstantVector::GetData<bool>(result);
+			*result_data = ConstantVector::IsNull(input);
+
+			break;
+		}
+
+		default : {
+			// TODO: use UnifiedVectorFormat instead
+			throw InvalidInputException("is.na() only handles constant and flat vectors");
+			break;
+		}
 	}
 
-	idx_t base_idx = 0;
-	auto entry_count = ValidityMask::EntryCount(count);
-	for (idx_t entry_idx = 0; entry_idx < entry_count; entry_idx++) {
-		auto validity_entry = mask.GetValidityEntry(entry_idx);
-		idx_t next = MinValue<idx_t>(base_idx + ValidityMask::BITS_PER_VALUE, count);
-
-		if (ValidityMask::AllValid(validity_entry)) {
-			// all valid: check with isnan()
-			for (; base_idx < next; base_idx++) {
-				result_data[base_idx] = false;
-			}
-		} else if (ValidityMask::NoneValid(validity_entry)) {
-			// None valid:
-			for (; base_idx < next; base_idx++) {
-				result_data[base_idx] = true;
-			}
-		} else {
-			// partially valid: need to check individual elements for validity
-			idx_t start = base_idx;
-			for (; base_idx < next; base_idx++) {
-				result_data[base_idx] = !ValidityMask::RowIsValid(validity_entry, base_idx - start);
-			}
-		}
-	}
 }
 
 
