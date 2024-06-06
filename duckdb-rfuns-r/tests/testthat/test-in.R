c2b9d8f79f391991c1be5c9683dc83fd794e73f4
Romain Francois <romain@tada.science>
2024-06-06 07:02:23 +0000

    more tests

--- duckdb-rfuns-r/tests/testthat/test-in.R
+++ duckdb-rfuns-r/tests/testthat/test-in.R
@@ -1,3 +1,14 @@
 test_that("%in%", {
-  expect_rfuns_in(c(1, 2, 3), c(2, 5))
+  expect_rfuns_in(c(1, 2), c(2, 5))
+  expect_rfuns_in(c(1, 2, NA), c(2, 5))
+  expect_rfuns_in(c(1, 2), c(2, 5, NA))
+
+  expect_rfuns_in(c(NA_real_), c(NA_real_))
+
+  expect_rfuns_in(c(1, 2), c(2L, 5L))
+  expect_rfuns_in(c(1L, 2L), c(2, 5))
+  expect_rfuns_in(c(1L, 2L), c(2L, 5L))
+
+  skip("failing for now")
+  expect_rfuns_in(c(NA_real_), c(2, 5))
 })
