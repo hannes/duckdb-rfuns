cc9300d4948bfa222bbd6300e778257cfdaf7c9f
Romain Francois <romain@tada.science>
2024-06-17 09:22:48 +0000

    more tests

--- duckdb-rfuns-r/tests/testthat/test-in.R
+++ duckdb-rfuns-r/tests/testthat/test-in.R
@@ -5,11 +5,47 @@ test_that("%in%", {
   expect_rfuns_in(c(1, 2), c(2, 5, NA_real_))
   expect_rfuns_in(c(NA_real_), c(NA_real_))
 
+  # <lgl> %in% <lgl>
+  expect_rfuns_in(c(TRUE, FALSE), c(TRUE))
+  expect_rfuns_in(c(TRUE, FALSE), c(NA))
+  expect_rfuns_in(c(NA, TRUE), c(FALSE))
+  expect_rfuns_in(c(NA), c(NA))
+
+  # <lgl> %in% <int>
+  expect_rfuns_in(c(TRUE, FALSE), c(1L))
+  expect_rfuns_in(c(TRUE, FALSE), c(NA_integer_))
+  expect_rfuns_in(c(NA, TRUE), c(42L))
+  expect_rfuns_in(c(NA), c(NA_integer_))
+
+  # <int> %in% <lgl>
+  expect_rfuns_in(c(1L, 3L), c(TRUE))
+  expect_rfuns_in(c(1L, 0L), c(NA))
+  expect_rfuns_in(c(NA_integer_, 0L, 32L), c(FALSE))
+  expect_rfuns_in(c(NA_integer_), c(NA))
+
   # <int> %in% <int>
   expect_rfuns_in(c(1L, 2L), c(2L, 5L))
   expect_rfuns_in(c(1L, 2L, NA_integer_), c(2L, 5L))
   expect_rfuns_in(c(1L, 2L), c(2L, 5L, NA_integer_))
   expect_rfuns_in(c(NA_integer_), c(NA_integer_))
+
+  # <dbl> %in% <int>
+  expect_rfuns_in(c(1, 2), c(2L, 5L))
+  expect_rfuns_in(c(1, 2, NA_real_), c(2L, 5L))
+  expect_rfuns_in(c(1, 2), c(2L, 5L, NA_integer_))
+  expect_rfuns_in(c(NA_real_), c(NA_integer_))
+
+  # <dbl> %in% <lgl>
+  expect_rfuns_in(c(1, 3), c(TRUE))
+  expect_rfuns_in(c(1, 0), c(NA))
+  expect_rfuns_in(c(NA_real_, 0, 1, 34), c(FALSE))
+  expect_rfuns_in(c(NA_real_), c(NA))
+
+  # <lgl> %in% <dbl>
+  expect_rfuns_in(c(TRUE, FALSE), c(1))
+  expect_rfuns_in(c(TRUE, FALSE), c(NA_real_))
+  expect_rfuns_in(c(NA, TRUE), c(42))
+  expect_rfuns_in(c(NA), c(NA_real_))
 })
 
 test_that("%in% skipped", {
