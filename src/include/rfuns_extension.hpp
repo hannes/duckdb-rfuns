bc9c265a2cc37660fee4529703c0af50e2b818bd
Romain Francois <romain@tada.science>
2024-10-07 13:10:04 +0000

    as_character(BOOLEAN)

--- src/include/rfuns_extension.hpp
+++ src/include/rfuns_extension.hpp
@@ -62,6 +62,7 @@ ScalarFunctionSet base_r_gte();
 ScalarFunctionSet base_r_is_na();
 ScalarFunctionSet base_r_as_integer();
 ScalarFunctionSet base_r_as_numeric();
+ScalarFunctionSet base_r_as_character();
 
 ScalarFunctionSet base_r_in();
 
